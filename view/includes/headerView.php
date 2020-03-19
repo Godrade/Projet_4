1eda2ce5b3c4ef8996c8ef9c61116e70518c15d6
Godrade <jorisr@hotmail.fr>
2020-03-19 12:21:23 +0100

    CKEDITOR
    Ajout du CKEDITOR

--- view/includes/headerView.php
+++ view/includes/headerView.php
@@ -13,6 +13,7 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
         <link rel="stylesheet" href="public/css/login.css">
         <link rel="stylesheet" href="public/css/style.css">
+        <script src="vendor/ckeditor/ckeditor-full/ckeditor.js"></script>
     </head>
 <body>
     <nav class="menu">

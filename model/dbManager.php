1dadf93b835c102e890bc57d5cabfa3dd12e13e2
Godrade <jorisr@hotmail.fr>
2020-05-20 15:42:16 +0200

    Update dbManager.php

--- model/dbManager.php
+++ model/dbManager.php
@@ -10,8 +10,8 @@ class dbManager {
 	//Setter
 	public function setDb(){
 		$this->_db = new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', '');
-		//$this->_db = new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', 'XWD_u43TY8u309Of34H0');
-		//$this->_db = new PDO('mysql:host=sylensfrywopen.mysql.db;dbname=sylensfrywopen;charset=utf8', 'sylensfrywopen', 'Diable666');
+		//$this->_db = new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', '');
+		//$this->_db = new PDO('mysql:host=sylensfrywopen.mysql.db;dbname=sylensfrywopen;charset=utf8', 'sylensfrywopen', '');
 	}
 
 	//Getter
